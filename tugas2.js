66d5f27b7c5878d312ac2e379922eefc38372d4d
Ridwan <71011389+riidwanadiitya@users.noreply.github.com>
2021-01-04 08:50:46 +0700

    TUGAS 2 JS

---
+++ tugas2.js
@@ -0,0 +1,11 @@
+var umur = 24;
+var nama = "Ridwan"
+var tinggi = true;
+var istri = null;
+var hobi = ["Berenang, Nonton, Main"]
+
+console.log(umur);
+console.log(nama);
+console.log(tinggi);
+console.log(istri);
+console.log(hobi);
